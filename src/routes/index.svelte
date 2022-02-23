c2e85ccd5772c7134f51497c0a5575710037e5f0
d20cay <d20cayt@gmail.com>
2022-02-23 17:40:07 +0100

    Adds first test version

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import TwitterIcon from "../components/TwitterIcon.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="" class="uk-icon-button">
+	<TwitterIcon/>
+</a>
+<a href="" class="uk-icon-button" uk-icon="twitter">
+</a>
+<span class="uk-margin-small-right" uk-icon="icon: check; ratio: 2"></span>
+<span uk-icon="icon: check; ratio: 3.5"></span>
